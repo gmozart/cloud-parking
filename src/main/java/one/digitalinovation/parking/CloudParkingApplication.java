6e56aaadf5a1ae8409d8598d09e2940eeb774cb3
Gleison Mozart <mozart.gleison@gmail.com>
2022-11-28 17:57:32 -0300

    Configuration

--- src/main/java/one/digitalinovation/parking/CloudParkingApplication.java
+++ src/main/java/one/digitalinovation/parking/CloudParkingApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CloudParkingApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CloudParkingApplication.class, args);
 	}
 
