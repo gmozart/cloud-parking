1f2b8603926e2aba4c813f06361cf6d4b0e964b7
Gleison Mozart <mozart.gleison@gmail.com>
2022-11-29 01:20:01 -0300

    Test Heroku

--- src/main/java/one/digitalinovation/parking/controller/ParkingController.java
+++ src/main/java/one/digitalinovation/parking/controller/ParkingController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/parking")
+@RequestMapping(value = "/")
 public class ParkingController {
 
     private final ParkingService parkingService;
