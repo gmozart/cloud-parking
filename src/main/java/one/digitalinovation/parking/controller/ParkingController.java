91b6989c86db2f876a3ad8f1190f545d96512b6a
Gleison Mozart <mozart.gleison@gmail.com>
2022-11-29 01:23:58 -0300

    Corrected error Endpoint

--- src/main/java/one/digitalinovation/parking/controller/ParkingController.java
+++ src/main/java/one/digitalinovation/parking/controller/ParkingController.java
@@ -4,13 +4,14 @@ import one.digitalinovation.parking.controller.dto.ParkingDTO;
 import one.digitalinovation.parking.controller.mapper.ParkingMapper;
 import one.digitalinovation.parking.model.Parking;
 import one.digitalinovation.parking.service.ParkingService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/")
+@RequestMapping(value = "/parking")
 public class ParkingController {
 
     private final ParkingService parkingService;
@@ -21,6 +22,7 @@ public class ParkingController {
         this.parkingMapper = parkingMapper;
     }
 
+    @GetMapping
     public List<ParkingDTO> findAll(){
         List<Parking> parkingList = parkingService.findAll();
         List<ParkingDTO> result = parkingMapper.toParkingDTOList(parkingList);
