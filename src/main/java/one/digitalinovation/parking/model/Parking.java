52f36f437ba97af076ce66bfe4a2a5b6d82967fe
Gleison Mozart <mozart.gleison@gmail.com>
2022-11-28 22:51:10 -0300

    Created Class Parking

---
+++ src/main/java/one/digitalinovation/parking/model/Parking.java
@@ -0,0 +1,17 @@
+package one.digitalinovation.parking.model;
+
+import java.time.LocalDateTime;
+
+public class Parking {
+
+    private String id;
+    private String license;
+    private String state;
+    private String model;
+    private String color;
+    private LocalDateTime entryDate;
+    private LocalDateTime exitDate;
+    private Double bill;
+
+    
+}
