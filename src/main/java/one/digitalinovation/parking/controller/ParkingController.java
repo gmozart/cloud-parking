37a200311bc6f5087e5e8e7ebd7579f9d4414840
Gleison Mozart <mozart.gleison@gmail.com>
2022-11-29 01:12:50 -0300

    Created the ParkingMapper class taht performs the mapping of the domain object to the representation object

--- src/main/java/one/digitalinovation/parking/controller/ParkingController.java
+++ src/main/java/one/digitalinovation/parking/controller/ParkingController.java
@@ -1,16 +1,29 @@
 package one.digitalinovation.parking.controller;
 
+import one.digitalinovation.parking.controller.dto.ParkingDTO;
+import one.digitalinovation.parking.controller.mapper.ParkingMapper;
+import one.digitalinovation.parking.model.Parking;
 import one.digitalinovation.parking.service.ParkingService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/parking")
 public class ParkingController {
 
     private final ParkingService parkingService;
+    private final ParkingMapper parkingMapper;
 
-    public ParkingController(ParkingService parkingService) {
+    public ParkingController(ParkingService parkingService, ParkingMapper parkingMapper) {
         this.parkingService = parkingService;
+        this.parkingMapper = parkingMapper;
+    }
+
+    public List<ParkingDTO> findAll(){
+        List<Parking> parkingList = parkingService.findAll();
+        List<ParkingDTO> result = parkingMapper.toParkingDTOList(parkingList);
+        return result;
     }
 }
